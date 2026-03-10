e692f13f6d000f3e4d5ecd17ca763986dd4bdbdf
DIVYABHARATHI E <iamdivyabharathi9008@gmail.com>
2026-03-10 16:14:25 +0530

    Add solution for finding subarray with given sum

---
+++ GeeksForGeeks/SubarrayWithGivenSum.java
@@ -0,0 +1,26 @@
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        int start = 0;
+        int sum = 0;
+
+        for(int end = 0; end < n; end++){
+            sum += arr[end];
+
+            while(sum > s && start <= end){
+                sum -= arr[start];
+                start++;
+            }
+
+            if(sum == s){
+                result.add(start + 1);
+                result.add(end + 1);
+                return result;
+            }
+        }
+
+        result.add(-1);
+        return result;
+    }
+}
